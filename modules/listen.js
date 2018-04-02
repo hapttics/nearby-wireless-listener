8d3f6c6c0dff7dce99dd7c07b6455c2a4a8a31dd
miballe <miballe@outlook.com>
2018-04-02 17:05:44 +0100

    Isolated captured lines

--- modules/listen.js
+++ modules/listen.js
@@ -6,7 +6,7 @@
 */
 
 /* -- Used Packages -- */
-var config = require('../config');
+// var config = require('../config');
 var spawn = require('child_process').spawn;
 
 // TODO: If the list of fields is changed, ensure the right number is checked down in the bufvalues.length variable.
@@ -40,7 +40,28 @@ child.stdout.on('data', function(buf) {
 	// TODO: A buffer instance may contain one or more lines. At this point this has to be recognized and nest
 	//       all below actions to process each separately.
 	buftoks.forEach(wLineItem => {
-		console.log("[INF] [RAW] " + wLineItem.replace(/[\n\r]/g, ''));
+            if(wLineItem != "") {
+                // console.log("[INF] [RAW] " + wLineItem);
+                var bufvalues = wLineItem.split(",");
+                var logentry = {};
+                if(bufvalues.length == 9) {
+                    logentry = {
+                      "timestamp": Number(bufvalues[0]), // Transform to JS epoch time (*1000)
+                      "mac": bufvalues[1],
+                      "dbm": Number(bufvalues[2]),
+                      "wlansa": bufvalues[3],
+                      "is2ghz": Number(bufvalues[4]) == 1 ? true : false,
+                      "is5ghz": Number(bufvalues[5]) == 1 ? true : false,
+                      "fctype": Number(bufvalues[6]),
+                      "fcsubtype": Number(bufvalues[7]),
+                      "saresolved": bufvalues[8]
+                    };
+                  console.log("%j", logentry);
+                }
+                else {
+                    console.log("[WRG] Invalid number of bufvalues");
+                }
+            }
 	});
 	
 	// var logentry = '';
